a74f0e95dd091f9a1ade5a5a30609a020f4b701d
Eduardo <edumeirelles@yahoo.com.br>
2021-07-05 22:30:06 -0300

    add bio

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from werkzeug.utils import validate_arguments
 
 app = Flask(__name__)
 
-user = 'jgyypnad'
-password = 'lollsSe6Kb0FaBQvpnainS9KUHzVXdfw'
+user = 'nribskms'
+password = '5vzaXTm4whpCpVMys8SDYWwzBJS_n3Fr'
 host = 'tuffi.db.elephantsql.com'
-database = 'jgyypnad'
+database = 'nribskms'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}/{database}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -25,10 +25,11 @@ class Formula1(db.Model):
     pontos = db.Column(db.Float, nullable=False)
     podiums = db.Column(db.Integer, nullable=False)
     campeonatos = db.Column(db.Integer, nullable=False)
+    bio = db.Column(db.String(4000), nullable=False)
     imagem_url = db.Column(db.String(255), nullable=False)
 
 
-    def __init__(self, nome, vitorias, poles, gp_disputados, pontos, podiums, campeonatos, imagem_url):
+    def __init__(self, nome, vitorias, poles, gp_disputados, pontos, podiums, campeonatos, bio, imagem_url):
         self.nome = nome
         self.vitorias = vitorias
         self.poles = poles
@@ -36,11 +37,12 @@ class Formula1(db.Model):
         self.pontos = pontos
         self.podiums = podiums
         self.campeonatos = campeonatos
+        self.bio = bio
         self.imagem_url = imagem_url
     
     @staticmethod
     def read_all():
-        return Formula1.query.order_by(Formula1.id.asc()).all()
+        return Formula1.query.order_by(Formula1.vitorias.desc()).all()
 
     @staticmethod
     def read_single(registro_id):
@@ -55,7 +57,7 @@ class Formula1(db.Model):
         
         
     
-    def update(self, novo_nome, novo_vitorias, novo_poles, novo_gp_disputados, novo_pontos, novo_podiums, novo_campeonatos, novo_imagem_url):
+    def update(self, novo_nome, novo_vitorias, novo_poles, novo_gp_disputados, novo_pontos, novo_podiums, novo_campeonatos, novo_bio, novo_imagem_url):
         self.nome = novo_nome
         self.vitorias = novo_vitorias
         self.poles = novo_poles
@@ -63,6 +65,7 @@ class Formula1(db.Model):
         self.pontos = novo_pontos
         self.podiums = novo_podiums
         self.campeonatos = novo_campeonatos
+        self.bio = novo_bio
         self.imagem_url = novo_imagem_url
 
         self.save()
@@ -95,11 +98,11 @@ def create():
     erro = "Preencha todos os campos"
     if request.method == 'POST':
         form = request.form
-        if not form['nome'] or not form['vitorias'] or not form['poles'] or not form['gp_disputados'] or not form['pontos'] or not form['podiums'] or not form['campeonatos'] or not form['imagem_url']:
+        if not form['nome'] or not form['vitorias'] or not form['poles'] or not form['gp_disputados'] or not form['pontos'] or not form['podiums'] or not form['campeonatos'] or not form['bio'] or not form['imagem_url']:
             
             return render_template('erro.html', id_atribuido=id_atribuido, erro=erro)
 
-        registro = Formula1(form['nome'], form['vitorias'], form['poles'], form['gp_disputados'], form['pontos'], form['podiums'], form['campeonatos'], form['imagem_url'])
+        registro = Formula1(form['nome'], form['vitorias'], form['poles'], form['gp_disputados'], form['pontos'], form['podiums'], form['campeonatos'], form['bio'], form['imagem_url'])
         
         registro.save()
 
@@ -115,11 +118,11 @@ def update(registro_id):
 
     if request.method == 'POST':
         form = request.form
-        if not form['nome'] or not form['vitorias'] or not form['poles'] or not form['gp_disputados'] or not form['pontos'] or not form['podiums'] or not form['campeonatos'] or not form['imagem_url']:
+        if not form['nome'] or not form['vitorias'] or not form['poles'] or not form['gp_disputados'] or not form['pontos'] or not form['podiums'] or not form['campeonatos'] or not form['bio'] or not form['imagem_url']:
 
             return render_template('erro.html', registro=registro, erro=erro)
 
-        registro.update(form['nome'], form['vitorias'], form['poles'], form['gp_disputados'], form['pontos'], form['podiums'], form['campeonatos'], form['imagem_url'])
+        registro.update(form['nome'], form['vitorias'], form['poles'], form['gp_disputados'], form['pontos'], form['podiums'], form['campeonatos'], form['bio'], form['imagem_url'])
 
         sucesso = True
     
